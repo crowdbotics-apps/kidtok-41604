8e71ce3e773f38f8b3eebee9efd0bc3105de0ca0
Crowdbotics <team@crowdbotics.com>
2023-06-19 18:25:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kidtok-41604",
+  url: "https://kidtok-41604.botics.co",
+  api: "https://kidtok-41604.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
